bbb4be7d99d1742ccf6bbb96b36e2027f715d298
Cleyton Alves <cleyton1986@gmail.com>
2020-07-24 19:28:08 -0300

    Finishing the routes and completing the challenge

--- src/app.js
+++ src/app.js
@@ -10,20 +10,14 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (response) => {
-  return response.json(repositories)
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
 
-  const repository = {
-    id: uuid(),
-    title,
-    url,
-    techs,
-    likes: 0,
-  }
+  const repository = { id: uuid(), title, url, techs, likes: 0, }
 
   repositories.push(repository);
   return response.json(repository);
@@ -31,12 +25,33 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository no found." })
+  }
+  const repository = { id, title, url, techs, likes: repositories[repositoryIndex].likes };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  const { id } = request;
-  const repository = 
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository no found." })
+  }
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
@@ -46,7 +61,6 @@ app.post("/repositories/:id/like", (request, response) => {
   if (!repository) {
     return response.status(400).send()
   }
-
   repository.likes += 1;
 
   return response.json(repository);
